68f10faddaaa9255abd1cb0a2c4d65d90b5ad20d
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2022-08-28 12:51:56 +0000

    Stamp & Document - Ragdoll Joint

--- addons/ActiveRGR/Scripts/ActiveRagdollJoint.gd
+++ addons/ActiveRGR/Scripts/ActiveRagdollJoint.gd
@@ -1,28 +1,46 @@
 extends Generic6DOFJoint
 
+"""
+	Active Ragdolls - Ragdoll Joint
+		by Nemo Czanderlitch/Nino Čandrlić
+			@R3X-G1L       (godot assets store)
+			R3X-G1L6AME5H  (github)
+
+	This is the core of the active ragdolls. This joint attempts to match its own rotation with that of
+	the animation skeleton, creating the active radolls we al know and love.
+"""
+
+## The skeleton that plays animations
 export (NodePath) var animation_skeleton
 var TARGET_SKELETON : Skeleton
+
+## The bones A & B, which are ajointed
 export (int) var BONE_A_INDEX : int = -1
 export (int) var BONE_B_INDEX : int = -1
-export(float) var matching_velocity_multiplier = 1  ## HOW FAST, AND WITH WHICH FORCE THE JOINT MOTOR TRIES TO SNAP INTO POSITION
+
+
+## HOW FAST, AND WITH WHICH FORCE THE JOINT MOTOR TRIES TO SNAP INTO POSITION
+export(float) var matching_velocity_multiplier = 1
 
 
 
+"""
+	RUN INITIAL CHECKS AND INITIALIZE JOINT
+"""
 func _ready() -> void:
 	if not Engine.editor_hint:
-		### BLINDLY FOLLOWING THE THE CODE FOR THIS EXTENSION
-		## https://gist.github.com/mstevenson/7b85893e8caf5ca034e6 ...AT LINE 66
-		## COULD BE SPARADIC MOVEMENT IN RAGDOLL  
+		""" BLINDLY FOLLOWING THE THE CODE FROM THIS UNITY EXTENSION
+		 		https://gist.github.com/mstevenson/7b85893e8caf5ca034e6 """
+		## CAN CAUSE SPORRADIC MOVEMENT ON RUNTIME
 		#self.set_param_x(Generic6DOFJoint.PARAM_ANGULAR_FORCE_LIMIT, 9999999)
 		#self.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_FORCE_LIMIT, 9999999)
 		#self.set_param_z(Generic6DOFJoint.PARAM_ANGULAR_FORCE_LIMIT, 9999999)
-		
 		self.set_param_x(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_FORCE_LIMIT, 9999999)
 		self.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_FORCE_LIMIT, 9999999)
 		self.set_param_z(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_FORCE_LIMIT, 9999999)
+
 		assert(self.get_parent() is Skeleton, "The Ragdoll Bone[%s] is supposed to be a child of a Skeleton" % [self.name])
-		
-		
+
 		TARGET_SKELETON = self.get_node_or_null(animation_skeleton)
 		if TARGET_SKELETON:  ### IF ANIMATED SKELETON EXISTS
 			trace_skeleton(true)
@@ -39,7 +57,7 @@ func _ready() -> void:
 
 
 """
-ENABLE/DISABLE ANIMATED SKELETON TRACING
+	ENABLE/DISABLE ANIMATED SKELETON TRACING
 """
 func trace_skeleton( value : bool ) -> void:
 	self.set_physics_process(value)
@@ -48,19 +66,21 @@ func trace_skeleton( value : bool ) -> void:
 
 
 """
-APPLY FORCES TO THE JOINTS TO MAKE THEM MATCH THE ANIMATED SKELETON
+	APPLY FORCES TO THE JOINTS TO MAKE THEM MATCH THE ANIMATED SKELETON
 """
 func _physics_process(delta: float) -> void:
 	if not Engine.editor_hint:
 		var target_rotation : Basis = TARGET_SKELETON.get_bone_global_pose(BONE_B_INDEX).basis.inverse() * self.get_parent().get_bone_global_pose(BONE_B_INDEX).basis
-		
 		var target_velocity : Vector3 = target_rotation.get_euler() * matching_velocity_multiplier
+
 		self.set_param_x(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, target_velocity.x)
 		self.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, target_velocity.y)
 		self.set_param_z(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, target_velocity.z)
 
 
-######################################
+"""
+	HELPER FOR SETING THE SAME VALUE FOR THE SAME PARAMETER ACROSS ALL AXIS
+"""
 func _declare_flag_for_all_axis( param : int, value : bool ) -> void:
 	self.set_flag_x(param, value)
 	self.set_flag_y(param, value)
